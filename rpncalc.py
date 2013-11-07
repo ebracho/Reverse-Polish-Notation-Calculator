4e1548905f516946debfab0ee4357722c50afd9e
Kazlock <ebracho@mail.csuchico.edu>
2013-11-07 12:25:39 -0800

    Got rid of useless in_numeric func

--- rpncalc.py
+++ rpncalc.py
@@ -1,14 +1,6 @@
 import sys
 import operator
 
-def is_numeric(x):
-
-    try:
-        float(x)
-        return True
-    except:
-        return False
-
 operand_stack = []
 inp = raw_input("> ")
 inp_list = inp.split(' ')
@@ -21,23 +13,24 @@ ops = {'+': operator.add,
 
 for i in inp_list:
 
-    if is_numeric(i):
-        operand_stack.append(float(i))
+    try: operand_stack.append(float(i))
 
-    elif i in {'+', '-', '*','/','^'}:
+    except: 
 
-        if len(operand_stack) < 2: 
-            print "Error: Not enough operands."
-            exit()   
+        if i in {'+', '-', '*','/','^'}:
+
+            if len(operand_stack) < 2: 
+                print "Error: Not enough operands."
+                exit()   
+
+            else:
+                right = operand_stack.pop()
+                left = operand_stack.pop()
+                operand_stack.append(ops[i](left, right))
 
         else:
-            right = operand_stack.pop()
-            left = operand_stack.pop()
-            operand_stack.append(ops[i](left, right))
-
-    else:
-        print "Error: %s is not a valid argument." % i
-        exit()
+            print "Error: %s is not a valid argument." % i
+            exit()
 
 if len(operand_stack) != 1:
     print "Error: not enough operators"
