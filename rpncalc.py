5bf37f6c897e7370b7a9f9c45b50969d6584af76
Kazlock <ebracho@mail.csuchico.edu>
2013-11-07 21:32:02 -0800

    No longer gives up so easy

--- rpncalc.py
+++ rpncalc.py
@@ -1,8 +1,7 @@
 import sys
 import operator
 
-operand_stack = []
-inp_list = raw_input("> ").split(' ')
+print "Reverse Polish Notation Calculator. Type 'quit' to exit."
 
 ops = {'+': operator.add,
        '-': operator.sub,
@@ -10,24 +9,49 @@ ops = {'+': operator.add,
        '/': operator.div,
        '^': pow}
 
-for i in inp_list:
+def calculate(inp_list, operand_stack):
 
-    try: operand_stack.append(float(i))
+    for i in inp_list:
 
-    except ValueError: 
-        if i in ops:
-            if len(operand_stack) < 2: 
-                print "Error: Too many operators."
-                exit()   
+        try: operand_stack.append(float(i))
+
+        except ValueError: 
+            if i in ops:
+                if len(operand_stack) < 2: 
+                    print "Error: Too many operators. Resetting."
+                    return ['reset']
+                else:
+                    right = operand_stack.pop()
+                    left = operand_stack.pop()
+                    operand_stack.append(ops[i](left, right))
+            elif i == '':
+                return operand_stack
+            elif i == 'quit':
+                exit()
             else:
-                right = operand_stack.pop()
-                left = operand_stack.pop()
-                operand_stack.append(ops[i](left, right))
-        else:
-            print "Error: %r is not a valid argument." % i
-            exit()
-if len(operand_stack) != 1:
-    print "Error: Not enough operators"
-    exit()
+                print "Error: %r is not a valid argument. Resetting." % i
+                return ['reset']
 
-print operand_stack[0]
+    return operand_stack
+
+operand_stack = []
+prompt = "> "
+
+while(True):
+    inp_list = []
+    inp_list.extend(raw_input(prompt).split(' '))
+
+    operand_stack = calculate(inp_list, operand_stack)               
+
+    if operand_stack[0] == 'reset':
+        operand_stack[:] = []
+        prompt = "> "
+
+    elif len(operand_stack) != 1:
+        print "Error: Not enough operators. Resetting."
+        operand_stack[:] = []
+        prompt = "> "
+        continue
+        
+
+    else :prompt = str(operand_stack[0]) + ' '
